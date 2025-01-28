bf113788fa2abb4a93ee0388a0426b1df44fb07d
JanosToth1989 <janos.toth@humda.hu>
2025-01-28 18:18:04 +0000

    Change basestack config access

--- can_teleop/launch/traj_recorder.launch.py
+++ can_teleop/launch/traj_recorder.launch.py
@@ -1,22 +1,23 @@
 from launch import LaunchDescription, launch_description_sources
-from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
-from launch_ros.actions import Node
-from launch.actions import SetEnvironmentVariable, ExecuteProcess, IncludeLaunchDescription
+from launch_ros.actions import Node, DeclareLaunchArgument
+from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, EnvironmentVariable
 from ament_index_python.packages import get_package_share_directory
 import os
 
 
 def generate_launch_description():
-    BS_config_base = os.path.join(
-                        get_package_share_directory("system-configuration"),
-                        "config",
+    config_folder_path = LaunchConfiguration('config_folder')
+    BS_config_base = PathJoinSubstitution([
+                        config_folder_path,
                         "BS",
-                    )
-    BS_config_file = os.path.join(
-        BS_config_base,
-        "config.yaml",
-    )
+                    ])
+    BS_config_file = PathJoinSubstitution([
+                        BS_config_base,
+                        "config.yaml",
+                    ])
     return LaunchDescription([
+        DeclareLaunchArgument('config_folder', default_value="system_config/config/"),
         SetEnvironmentVariable(name='ROS2_BS_CONFIG_BASE', value=BS_config_base),
         SetEnvironmentVariable(name='ROS2_BS_CONFIG_PATH', value=BS_config_file),
         SetEnvironmentVariable(name='ROS_AUTOMATIC_DISCOVERY_RANGE', value='LOCALHOST'),
