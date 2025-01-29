c4c574eae26e1645fe4b51026c414fd79eb94bc1
humdalabdesktop <humdalabdesktop@gmail.com>
2025-01-29 11:59:50 +0100

    import bugfix

--- can_teleop/launch/traj_recorder.launch.py
+++ can_teleop/launch/traj_recorder.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription, launch_description_sources
-from launch_ros.actions import Node, DeclareLaunchArgument
-from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription, DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, EnvironmentVariable
 from ament_index_python.packages import get_package_share_directory
 import os
